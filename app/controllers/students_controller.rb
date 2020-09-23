d01ed0088e9d66e1d52c1778962290510cd6f1fd
gerardmi24 <gerardmistretta@yahoo.com>
2020-09-23 16:52:38 -0400

    Complete and functioning but still has errors

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,24 +1,35 @@
 class StudentsController < ApplicationController
 
   def index
-    @students = Student.all
+    @student = Student.all
+  end
+  
+  def show
+    @student = Student.find(params[:id])
   end
 
   def new
     @student = Student.new
   end
 
-  def create
-  end
-
-  def show
-  end
-
   def edit
+    @student = Student.find(params[:id])
+  end
+
+  def create
+    @student = Student.new(student_params)
+    @student.save
+    redirect_to student_path(@student)
   end
 
   def update
+    @student = Student.find(params[:id])
     @student.update(student_params)
     redirect_to student_path(@student)
+  end
+
+  private
+  def student_params
+    params.require(:student).permit!
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,7 @@
 Rails.application.routes.draw do
-  # get 'school_classes/index'
 
-  # get 'school_classes/new'
-
-  # get 'school_classes/create'
-
-  # get 'school_classes/show'
-
-  # get 'school_classes/edit'
-
-  # get 'school_classes/update'
-
-  # get 'students/new'
-
-  # get 'students/create'
-
-  # get 'students/show'
-
-  # get 'students/edit'
-
-  # get 'students/update'
     resources :students, only: [:index, :new, :create, :show, :edit, :update]
     resources :school_classes, only: [:index, :new, :create, :show, :edit, :update]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
